d9606e69c48d97f6332922e32c993b561108047d
bijay <sigdelbjay@gmail.com>
2017-08-27 14:54:12 +0545

    How the module works

--- greet.js
+++ greet.js
@@ -1,4 +1,17 @@
-var greet = function(){
-    console.log('hi');
-}
-module.exports = greet;
+// (function(exports, require, module, __filename, __dirname){
+
+    var greet = function(){
+        console.log('hi');
+    }
+    module.exports = greet;
+
+// });
+
+// fn(module.exports, require, module, filename, dirname);
+
+// return module.exports;
+
+//require is a function that you pass a path to
+//module.exports is what require function returns
+//this works because your code is actually wrapped in a function that is
+//given these things as function parameter.
